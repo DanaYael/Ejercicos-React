d391af1307947cba1eebe693d3cee544a22c2da1
Dana Yael Albornoz <Yaelalbornoz23@gmail.com>
2024-12-11 15:26:39 -0300

    componente Hola

---
+++ EjerciciosReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Hola from "./componentes/Hola";
+
+function App() {
+  return (
+    <>
+      <main>
+        <Hola></Hola>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ EjerciciosReact/src/componentes/Hola.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Hola = () => {
+  return <h1 className="d-flex justify-content aling-center">Hellow word!</h1>;
+};
+
+export default Hola;
